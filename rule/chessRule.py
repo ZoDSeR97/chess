326a17daad9b32c8b731cd4ad317bac88bb415db
ZoDSeR SyLVaN <zodser97@gmail.com>
2020-03-06 13:09:48 -0500

    update rule

--- rule/chessRule.py
+++ rule/chessRule.py
@@ -7,12 +7,16 @@ class Rule:
         self.board = board
         self.moveList = piecesMoves
         self.piece = piece
+    
+    def Check(self):
+        self.removeList.clear()
 
 class checkPieces(Rule):
     def __init__(self, board, moveList, piece):
         super().__init__(board, moveList, piece)
 
     def Check(self):
+        super().Check()
         for i in self.moveList:
             if self.board[i[0]][i[1]].pieceOccupy.toString() != "0":
                 if self.board[i[0]][i[1]].pieceOccupy.alliance == self.piece.alliance:
@@ -24,9 +28,7 @@ class checkPieces(Rule):
                         self.removeList.append(i)
                 if len(self.moveList) == 0 or self.piece.toString() != "N" and self.piece.toString() != "K":
                     for j in self.moveList:
-                        if len(self.moveList) == len(self.removeList):
-                            break
-                        elif j in self.removeList:
+                        if j in self.removeList:
                             continue
                         if j[0] == i[0] == self.piece.x_coord:
                             if j[1] < i[1] < self.piece.y_coord:
@@ -40,14 +42,13 @@ class checkPieces(Rule):
                                 self.removeList.append(j)
                         elif j[1] > i[1] > self.piece.y_coord and j[0] < i[0] < self.piece.x_coord:
                             self.removeList.append(j)
-                        elif j[1] < i[1] < self.piece.y_coord and j[0] < i[0] < self.piece.x_coord:
+                        elif j[1] < i[1] < self.piece.y_coord and j[0] < i[0] < self.piece.x_coord:   
                             self.removeList.append(j)
-                        elif j[1] > i[1] > self.piece.y_coord and j[0] > i[0] > self.piece.x_coord:
+                        elif j[1] > i[1] > self.piece.y_coord and j[0] > i[0] > self.piece.x_coord:  
                             self.removeList.append(j)
-                        elif j[1] < i[1] < self.piece.y_coord and j[0] > i[0] > self.piece.x_coord:
+                        elif j[1] < i[1] < self.piece.y_coord and j[0] > i[0] > self.piece.x_coord:   
                             self.removeList.append(j)
         self.moveList = [i for i in self.moveList if i not in self.removeList]
-        self.removeList.clear()
 
 class enPassant(Rule):
 
